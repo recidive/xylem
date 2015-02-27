7481312746ea36c09f6530aecbbe3abda57be7c5
Henrique Recidive <recidive@gmail.com>
2015-02-27 14:39:51 -0300

    Calling init() in example.js.

--- example/basic.js
+++ example/basic.js
@@ -1,7 +1,7 @@
 var Server = require('../');
 var Memory = require('../lib/memory');
 
-// Create server,
+// Create server.
 var server = new Server();
 
 // Set adapters and create connections.
@@ -20,22 +20,24 @@ server.model('contact', {
   key: 'id'
 });
 
-// Get 'contact' model.
-var Contact = server.model('contact');
+server.init(function() {
+  // Get 'contact' model.
+  var Contact = server.model('contact');
 
-// Create 'john' contact instance.
-var john = new Contact({
-  id: 1,
-  name: 'John',
-  email: 'john@example.com'
-});
+  // Create 'john' contact instance.
+  var john = new Contact({
+    id: 1,
+    name: 'John',
+    email: 'john@example.com'
+  });
 
-// Save contact.
-john.save(function (error, john) {
-  console.log(john);
+  // Save contact.
+  john.save(function (error, john) {
+    console.log(john);
 
-  // Get a list of contacts.
-  Contact.list({}, function (error, contacts) {
-    console.log(contacts);
-  })
+    // Get a list of contacts.
+    Contact.list({}, function (error, contacts) {
+      console.log(contacts);
+    });
+  });
 });
