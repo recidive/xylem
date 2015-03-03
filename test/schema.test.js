dcb9559ca4053a9ab4e93b993beb9c11e0930b8c
Henrique Recidive <recidive@gmail.com>
2015-03-03 01:33:17 -0300

    Improving tests asserts and adding test for object and array fields.

--- test/schema.test.js
+++ test/schema.test.js
@@ -8,7 +8,9 @@ var sampleSchema = {
   fields: {
     id: 'number',
     name: 'string',
-    active: 'boolean'
+    active: 'boolean',
+    address: 'object',
+    phones: 'array'
   },
   key: 'id'
 };
@@ -24,13 +26,14 @@ describe('Schema#check', function(done) {
 
   it('should not allow a non numeric value on a number field', function(done) {
     var errors = schema.check({
-      id: 'abcd'
+      id: 'abcd',
+      name: 'John'
     });
 
     assert.ok(errors instanceof Array);
+    assert.equal(errors[0], 'Invalid value "abcd" for field "id".');
     done();
   });
-
 
   it('should not allow a non string value on a string field', function(done) {
     var errors = schema.check({
@@ -39,17 +42,44 @@ describe('Schema#check', function(done) {
     });
 
     assert.ok(errors instanceof Array);
+    assert.equal(errors[0], 'Invalid value "123" for field "name".');
     done();
   });
 
   it('should not allow a non boolean value on a boolean field', function(done) {
     var errors = schema.check({
       id: 123,
-      name: 123,
+      name: 'John',
       active: 'abcd'
     });
 
     assert.ok(errors instanceof Array);
+    assert.equal(errors[0], 'Invalid value "abcd" for field "active".');
+    done();
+  });
+
+  it('should not allow a non object value on an object field', function(done) {
+    var errors = schema.check({
+      id: 123,
+      name: 'John',
+      address: 'address string'
+    });
+
+    assert.ok(errors instanceof Array);
+    assert.equal(errors[0], 'Invalid value "address string" for field "address".');
+    done();
+  });
+
+
+  it('should not allow a non array value on an array field', function(done) {
+    var errors = schema.check({
+      id: 123,
+      name: 'John',
+      phones: 'phone string'
+    });
+
+    assert.ok(errors instanceof Array);
+    assert.equal(errors[0], 'Invalid value "phone string" for field "phones".');
     done();
   });
 
