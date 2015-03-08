bc9250a97413a04f2c5a341f9b1d91a6e3f99e88
Henrique Recidive <recidive@gmail.com>
2015-03-08 15:56:51 -0300

    Adding tests for Model.update() and Model.destroy() methods.

--- test/model.test.js
+++ test/model.test.js
@@ -111,21 +111,22 @@ describe('#get()', function() {
 
 });
 
+function createMultipleSamples(callback) {
+  async.each(samples, function(sample, next) {
+    Contact.create(sample, function (error, john) {
+      if (error) {
+        return next(error);
+      }
+      next();
+    });
+  }, callback);
+}
+
 describe('#list()', function() {
 
-  beforeEach(function(done) {
-    async.each(samples, function(sample, next) {
-      Contact.create(sample, function (error, john) {
-        if (error) {
-          return next(error);
-        }
-        next();
-      });
-    }, done);
-  });
+  beforeEach(createMultipleSamples);
 
   it('should list all items', function(done) {
-    // Get item with a criteria.
     Contact.list(function (error, contacts) {
       if (error) {
         return done(error);
@@ -139,7 +140,6 @@ describe('#list()', function() {
   });
 
   it('should list items that match a criteria object', function(done) {
-    // Get item with a criteria.
     Contact.list({
       id: sample.id
     },
@@ -152,6 +152,104 @@ describe('#list()', function() {
       assert.ok(contacts instanceof Array);
       assert.equal(contacts.length, 1);
       done();
+    });
+  });
+
+});
+
+describe('#update()', function() {
+
+  beforeEach(createMultipleSamples);
+
+  it('should update an item by its key', function(done) {
+    var newEmail = 'newemail@example.com';
+
+    Contact.update(sample.id, {
+      email: newEmail
+    },
+    function (error, contacts) {
+      if (error) {
+        return done(error);
+      }
+
+      assert.ok(contacts);
+      assert.ok(contacts instanceof Array);
+      assert.equal(contacts.length, 1);
+      assert.equal(contacts[0].email, newEmail)
+      done();
+    });
+  });
+
+  it('should update items that match a criteria', function(done) {
+    var newEmail = 'newemail@example.com';
+
+    Contact.update({
+      id: sample.id
+    },
+    {
+      email: newEmail
+    },
+    function (error, contacts) {
+      if (error) {
+        return done(error);
+      }
+
+      assert.ok(contacts);
+      assert.ok(contacts instanceof Array);
+      assert.equal(contacts.length, 1);
+      assert.equal(contacts[0].email, newEmail)
+      done();
+    });
+  });
+
+});
+
+describe('#destroy()', function() {
+
+  beforeEach(createMultipleSamples);
+
+  it('should destroy an item by its key', function(done) {
+    Contact.destroy(sample.id, function (error, contacts) {
+      if (error) {
+        return done(error);
+      }
+
+      assert.ok(contacts);
+      assert.ok(contacts instanceof Array);
+      assert.equal(contacts.length, 1);
+
+      Contact.get(sample.id, function (error, contact) {
+        if (error) {
+          return done(error);
+        }
+
+        assert.ifError(contact);
+        done();
+      });
+    });
+  });
+
+  it('should destroy items that match a criteria', function(done) {
+    Contact.destroy({
+      id: sample.id
+    },
+    function (error, contacts) {
+      if (error) {
+        return done(error);
+      }
+
+      assert.ok(contacts);
+      assert.ok(contacts instanceof Array);
+      assert.equal(contacts.length, 1);
+
+      Contact.get(sample.id, function (error, contact) {
+        if (error) {
+          return done(error);
+        }
+
+        assert.ifError(contact);
+        done();
+      });
     });
   });
 
